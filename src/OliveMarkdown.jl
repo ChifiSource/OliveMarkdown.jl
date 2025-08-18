6e19acac1a52eed280090f30903705ed4385a2f7
emmy <52672675+emmaccode@users.noreply.github.com>
2025-08-18 13:38:39 -0600

    documented package

--- src/OliveMarkdown.jl
+++ src/OliveMarkdown.jl
@@ -1,8 +1,44 @@
+"""
+Created in August, 2025 by
+[chifi - an open source software dynasty.](https://github.com/orgs/ChifiSource)
+- This software is MIT-licensed.
+### OliveMarkdown
+`OliveMarkdown` provides `Olive` with the ability to read, edit, and export markdown files as cells. 
+Load this into `Olive` and you'll be able to read and edit cells as markdown files!
+```julia
+# internal bindings
+`mak_cellstr`
+`olivemd_string`
+`imagecell_omd_str`
+`olivemd_save`
+`construct_mdcell`
+`parse_omd_cell`
+`is_invalid_cellstr`
+`read_olivemd`
+
+# olive bindings:
+build(c::Connection, cell::Cell{:md}, d::Directory)
+olive_read(cell::Cell{:md})
+olive_save(p::Project{<:Any}, pe::ProjectExport{:md})
+```
+"""
 module OliveMarkdown
 import Olive: olive_save, Project, Cell, build, olive_read
 import Olive.IPyCells: AbstractCell
 using Olive: Connection, Directory, style!, build_base_cell, ProjectExport, Component
+using Olive.Toolips.Components: base64img
 
+"""
+```julia
+make_cellstr(cell::Cell{<:Any}, celltype::Any) -> ::String
+```
+Makes a Markdown cell string for any cell. This will be written as a code block 
+    in the returned `String`. This is a convenience function mostly used
+    to make `olivemd_string` more convenient.
+```julia
+```
+- See also: `olivemd_string`, `OliveMarkdown`, `construct_mdcell`
+"""
 function make_cellstr(cell::Cell{<:Any}, celltype::Any)
     cellstr::String = """\n```$celltype
     $(cell.source)
@@ -13,6 +49,17 @@ function make_cellstr(cell::Cell{<:Any}, celltype::Any)
     return(cellstr)::String
 end
 
+"""
+```julia
+olivemd_string(cell::Cell{<:Any}) -> ::String
+```
+Turns a cell into a markdown capable string. All available cells for a `.md` file 
+will have a method binding to both this function and `construct_mdcell`, allowing 
+    that cell to then be read and wrote.
+```julia
+```
+- See also: `make_cellstr`, `OliveMarkdown`, `olivemd_save`
+"""
 olivemd_string(cell::Cell{<:Any}) = begin
     celltype = typeof(cell).parameters[1]
     return(make_cellstr(cell, celltype))::String
@@ -38,6 +85,16 @@ function olivemd_string(cell::Cell{:tomlvalues})
     make_cellstr(cell, "toml")
 end
 
+"""
+```julia
+imagecell_omd_str(cell::Cell{<:Any}) -> ::String
+```
+A convenience output binding for image cells that is used for both 
+    `:image` and `:imagero` cells.
+```julia
+```
+- See also: `olivemd_string`, `parse_omd_Cell`, `olivemd_save`, `OliveMarkdown`
+"""
 function imagecell_omd_str(cell::Cell{<:Any})
     fmt = lowercase(cell.source)
     outpimg = base64img("cell$(cell.id)", cell.outputs[2], fmt)
@@ -64,6 +121,15 @@ function olivemd_string(cell::Cell{:vimagero})
     make_cellstr(cell, "svg")
 end
 
+"""
+```julia
+olivemd_save(cells::Vector{Cell}, path::AbstractString; mdcellt::Type = Cell{:markdown}) -> ::Nothing
+```
+Saves a `Vector{Cell}` as markdown. `mdcellt` will replace the default markdown file cell type.
+```julia
+```
+- See also: `olivemd_string`, `OliveMarkdown`, `construct_mdcell`, `read_olivemd`
+"""
 function olivemd_save(cells::Vector{Cell}, path::AbstractString; mdcellt::Type = Cell{:markdown})
     if ~(isfile(path))
         touch(path)
@@ -83,6 +149,21 @@ function olivemd_save(cells::Vector{Cell}, path::AbstractString; mdcellt::Type =
     end
 end
 
+"""
+```julia
+construct_mdcell(type::Type{<:AbstractCell}, source::String, outputs::Any = nothing) -> ::Cell{<:Any}
+```
+Constructs a cell from a raw block string. This function is called with a block's name and what is 
+to be read. For instance, when a `code` cell is turned into a markdown string 
+it becomes a Julia code block:
+```julia
+# this is a julia code block
+```
+When it is read back, it is typed as :julia. So we have `construct_mdcell(type::Type{Cell{:julia}}, source::String, outputs::Any = nothing)` 
+bound to turn a `:julia`  cell into a `:code` cell.
+
+- See also: `olivemd_string`, `OliveMarkdown`, `construct_mdcell`, `read_olivemd`
+"""
 function construct_mdcell(type::Type{<:AbstractCell}, source::String, outputs::Any = nothing)
     type(source, outputs)
 end
@@ -100,14 +181,29 @@ function construct_mdcell(type::Type{Cell{:svg}}, source::String, outputs::Any =
 end
 
 function construct_mdcell(type::Type{Cell{:imgb64}}, source::String, outputs::Any = nothing)
-    splts = split(cell.source, "!|")
-    Cell{:image}(replace(splits[1], " " => "", "\n" => ""), join(splts[2:end], "!|"))
+    splits = split(source, "!|")
+    Cell{:image}(replace(splits[1], " " => "", "\n" => ""), join(splits[2:end], "!|"))
 end
 
 function olive_save(p::Project{<:Any}, pe::ProjectExport{:md})
+    if ~(contains(p[:path], ".md"))
+        p[:path] = p[:path] * ".md"
+    end
     olivemd_save(p[:cells], p[:path])
 end
 
+
+"""
+```julia
+parse_omd_cell(raw::AbstractString) -> ::Cell{<:Any}
+```
+Parses a codestring into a cell. This function is provided both the markdown 
+and code snippets for each cell. It then parses them into appropriate functions 
+    using `construct_mdcell`.
+```julia
+```
+- See also: `olivemd_string`, `OliveMarkdown`, `construct_mdcell`
+"""
 function parse_omd_cell(raw::AbstractString)
     code_found = findfirst("``", raw)
     if code_found == 1:2
@@ -130,6 +226,16 @@ function parse_omd_cell(raw::AbstractString)
     return(Cell{:markdown}(raw))
 end
 
+"""
+```julia
+is_invalid_cellstr(str::AbstractString) -> ::Bool
+```
+Checks if a `String` contains any characters aside from `'`, ` ` and `\\n`. If 
+    the string contains no other characters, returns `true`.
+```julia
+```
+- See also: `olivemd_string`, `OliveMarkdown`, `construct_mdcell`
+"""
 is_invalid_cellstr(str::AbstractString) = begin
     badvalues = ('\n', ' ', '`')
     isnothing(findfirst(x -> ~(x in badvalues), str))
